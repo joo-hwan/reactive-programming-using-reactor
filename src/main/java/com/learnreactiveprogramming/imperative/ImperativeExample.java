76d9842f249b842170fd0318b6063416496296b5
211947 <joohwan@ssg.com>
2022-06-14 09:43:02 +0900

    Initial commit

---
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -0,0 +1,24 @@
+package com.learnreactiveprogramming.imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImperativeExample {
+   public static void main(String[] args) {
+        var nameList = List.of("alex", "ben", "chloe", "adam", "adam");
+        var newNamesList = namesGreaterThanSize(nameList, 3);
+
+        System.out.println("newNamesList : " + newNamesList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> nameList, int size) {
+        var newNameList = new ArrayList<String>();
+
+        for(String name : nameList) {
+            if(name.length() > size && !newNameList.contains(name)) {
+                newNameList.add(name.toUpperCase());
+            }
+        }
+        return newNameList;
+    }
+}
