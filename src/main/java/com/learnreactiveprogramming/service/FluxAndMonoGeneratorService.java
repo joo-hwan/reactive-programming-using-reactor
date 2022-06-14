d7d79e5bbec5d53f8389f2c72791accd71468c68
joo-hwan <wnghksrla001@gmail.com>
2022-06-15 00:07:40 +0900

    flux and mono, operate map and filter

---
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -0,0 +1,50 @@
+package com.learnreactiveprogramming.service;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .log();
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("alex")
+                .log();
+    }
+
+    public Flux<String> namesFlux_map(int stringLength) {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                //.map(s -> s.toUpperCase())
+                .filter(s -> s.length() > stringLength)
+                .map(s -> s.length() + "-" + s)
+                .log();
+    }
+
+    public Flux<String> namesFlux_immutability() {
+        var namesFlux = Flux.fromIterable(List.of("alex", "ben", "chloe"));
+        namesFlux.map(String::toUpperCase);
+        return namesFlux;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> {
+                    System.out.println("Name is : " + name);
+                });
+
+        fluxAndMonoGeneratorService.namesMono()
+                .subscribe(name -> {
+                    System.out.println("Mono Name is : " + name);
+                });
+    }
+}
--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,42 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                //.expectNext("alex", "ben", "chloe")
+                //.expectNextCount(3)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_map() {
+        int stringLength = 3;
+
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux_map(stringLength);
+
+        StepVerifier.create(namesFlux)
+                .expectNext("4-ALEX", "5-CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        var nameFluxImmutability = fluxAndMonoGeneratorService.namesFlux_immutability();
+
+        StepVerifier.create(nameFluxImmutability)
+                //.expectNext("ALEX", "BEN", "CHLOE")
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
 }
